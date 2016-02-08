bebfbbe8a3a10138080a55eba9c2b72d7a3b0e19
Couther <Couther@outlook.com>
2016-02-08 23:03:46 +0800

    稍微完善了反作弊的客户端数据读取

--- src/main/java/net/mechgear/geekscraft/geekscraft/anticheat/ClientInformation.java
+++ src/main/java/net/mechgear/geekscraft/geekscraft/anticheat/ClientInformation.java
@@ -10,9 +10,19 @@ public class ClientInformation {
 	
 	ClientInformation(){
 		List<File> fl = new ReadFile().getFileList();
+		for(File f : fl){
+			info.put("SHA-1:"+GetSHA1.sha1Reader(f), pathWork(f));
+		}
+		for(File f : fl){
+			info.put("MD5:"+GetMD5.md5Reader(f), pathWork(f));
+		}
 		
 	}
 	
+	private String pathWork(File f){
+		String s = f.getAbsolutePath();
+		return s.substring(s.lastIndexOf(".minecraft")+11);
+	}
 	
 	Map<String,String> getClientInformation(){
 		return info;
--- src/main/java/net/mechgear/geekscraft/geekscraft/anticheat/GetSHA1.java
+++ src/main/java/net/mechgear/geekscraft/geekscraft/anticheat/GetSHA1.java
@@ -2,36 +2,37 @@ package net.mechgear.geekscraft.geekscraft.anticheat;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class GetSHA1 {
-
-	void fx(File f){
+	
+	static String sha1Reader(File f){
 		if (!f.isFile()) {
 			System.err.println("这不是个文件");
-			return;
+			return null;
 		}
-		
-		MessageDigest md = null;
-//		FileInputStream in = new FileInputStream(f);
-		long l = f.length()+1;
-		byte[] buffer = new byte[(int)l];
 		
 		try {
-			md = MessageDigest.getInstance("MD5");
+			FileInputStream in = new FileInputStream(f);
+			byte[] b = new byte[(int)f.length()];
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			md.update(b, 0, in.read(b)); 
+			in.close();
+			return new BigInteger(1, md.digest()).toString(16);
+			
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-			
-//			md.update(buffer, 0, len); 
 		
-//		in.close();
-		
-		BigInteger bigInt = new BigInteger(1, md.digest());
-//		md5.put(bigInt.toString(16), f.getPath());
-		
+		return null;
 	}
 	
 }
